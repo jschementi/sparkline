cb07310c7a7361a4d3d7f01cf478444242316e7f
Jimmy Schementi <jschementi@gmail.com>
2011-05-08 01:32:57 -0400

    Tweak sample tick generation to do all work on a background thread

--- Schementi.Controls.Demos.Sparkline/MainWindow.xaml.cs
+++ Schementi.Controls.Demos.Sparkline/MainWindow.xaml.cs
@@ -1,31 +1,43 @@
 ﻿using System;
 using System.Linq;
-using System.Windows;
-using System.Windows.Threading;
+using System.Threading;
 
 namespace Schementi.Controls.Demos.Sparkline {
     public partial class MainWindow {
+        static readonly object StopLock = new object();
         public MainWindow() {
             InitializeComponent();
 
-            var sparklines = Grid.Children.OfType<Controls.Sparkline>();
+            var sparklines = Grid.Children.OfType<Controls.Sparkline>().ToArray();
+            var sparklinesCount = sparklines.Length;
+            const int tickTime = 1000/2;
+            var stop = false;
+            WaitCallback tickLoop = wc => {
+                while (true) {
+                    var x = 100.0;
+                    for (var i = 0; i < sparklinesCount; i++) {
+                        var random = new Random();
+                        //x = random.Next(100, 200);
+                        x = x + (random.NextDouble() * 10 * random.NextDouble() * (random.NextDouble() < 0.5 ? -1 : 1));
+                        if (x < 1) x = Math.Abs(x) + 1;
+                        Console.WriteLine(x);
 
-            var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(0.5) };
-
-            var x = 100.0;
-            timer.Tick += (s, e) => {
-                foreach (var sparkline in sparklines) {
-                    var random = new Random();
-                    //x = random.Next(100, 200);
-                    x = x + (random.NextDouble() * 10 * random.NextDouble() * (random.NextDouble() < 0.5 ? -1 : 1));
-                    if (x < 1) x = Math.Abs(x) + 1;
-                    Console.WriteLine(x);
-                    sparkline.AddTimeValue(x);
+                        var i1 = i;
+                        var x1 = x;
+                        Dispatcher.BeginInvoke(new Action(() => sparklines[i1].AddTimeValue(x1)));
+                    }
+                    Thread.Sleep(tickTime);
+                    lock (StopLock) if (stop) return;
                 }
             };
+            Action start = () => ThreadPool.QueueUserWorkItem(tickLoop);
 
-            Loaded += (s, e) => timer.Start();
-            Unloaded += (s, e) => timer.Stop();
+            Loaded += (s, e) => start();
+            Unloaded += (s, e) => {
+                            lock (StopLock) {
+                                stop = true;
+                            }
+                        };
         }
     }
 }
